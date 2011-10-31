b9b1540c2f0a55a459583530cb473338bd652ba0
Eithan Shavit <oogiyot@gmail.com>
2011-10-30 17:41:45 -0700

    minor changes + comments

--- timer.rb
+++ timer.rb
@@ -1,18 +1,22 @@
-print "\n    <<<The Very Annoying Timer>>>\n\nBeep every how many minutes? (Default is 5 mins) : "
-BeepEveryMin = (temp = gets.chomp.to_i) > 0 ? temp : 5
+# This annoying timer beep every x minutes. It is also quietly counting minutes.
+# Timer will beep at the end of each interval. Beeping number of times to match current number of interval.
+
+defaultMins = 5 #Default value of minutes of for invalid input or ease of use.
+print "\n<<<The Very Annoying Timer>>>\n\nBeep every how many minutes? (Default is #{defaultMins} mins) : "
+BeepEveryMin = (temp = gets.chomp.to_i) > 0 ? temp : defaultMins #get interval from user or set default
 puts "\nBeeping every #{BeepEveryMin} minutes"
 puts
 segments = 1
 minutes = 0
-while (1) do
+while (1) do #never quit, just close window
 	
-	BeepEveryMin.times do
+	BeepEveryMin.times do #count minutes
 		print "\rElapsed: #{minutes}"
 		sleep 60
 		minutes += 1
 		
 	end
-	segments.times do
+	segments.times do #beep number of time according to current interval.
 		print "\a"
 		sleep 0.001
 	end
